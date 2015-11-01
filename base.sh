0f1d2618399e374cd6de511976b1e2c1087ae6b0
Eugene Wang <fengbaoxp@gmail.com>
2015-11-01 22:05:17 +0800

    修改bash.sh提示信息

--- base.sh
+++ base.sh
@@ -13,7 +13,7 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    read -p "执行这个脚本可能会覆盖home目录下已经存在的文件，确定要执行么？ (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         doIt;
