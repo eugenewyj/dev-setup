dc1827b49800792df69cd14d5899e751f1e4cece
Eugene Wang <fengbaoxp@gmail.com>
2017-10-01 10:06:09 +0800

    添加macdown软件

--- brew.sh
+++ brew.sh
@@ -72,6 +72,7 @@ brew cask install --appdir="/Applications" macpass
 brew cask install --appdir="/Applications" dictunifier
 brew cask install --appdir="/Applications" calibre
 brew cask install --appdir="/Applications" kindle
+brew cask install --appdir="/Applications" macdown
 
 ## 安装shadowsocksx(配置参考备忘)
 brew cask install --appdir="/Applications" shadowsocksx
